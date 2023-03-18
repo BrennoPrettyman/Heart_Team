7e57ac92bd37da7071f285aa06b5887426f5d36c
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-17 21:17:05 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Heart_Team;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Coração do seu Time!");
+        Console.WriteLine();
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("P");
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Write("A");
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("I");
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.Write("N");
+        Console.ResetColor();
+
+    }
+}
